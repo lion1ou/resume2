a8a078cae1e27b8fc7b8ef55030abfacd922cd28
lion <418120186@qq.com>
2017-03-09 22:59:57 +0800

    20170309-2259

---
+++ src/App.vue
@@ -0,0 +1,292 @@
+<template>
+    <div id="app">
+        <styleEditor ref="styleEditor" :code="currentStyle"></styleEditor>
+        <resumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></resumeEditor>
+    </div>
+</template>
+<script>
+import styleEditor from './components/styleEditor'
+import resumeEditor from './components/resumeEditor'
+
+export default {
+    name: 'app',
+    components: {
+        styleEditor,
+        resumeEditor
+    },
+    data() {
+        return {
+            intervalTime: 50,
+            currentStyle: '',
+            currentMarkdown: '',
+            enableHtml: false,
+            myStyle: [`/* Hello!  I'm lion1ou. 
+* 
+*  又到了一年招聘季，正所谓金三银四
+*  
+*  作为一个正想跳槽的前端er，怎么可以错过这个机会呢？
+*  
+*  要跳槽那肯定得有份简历吧！
+* 
+*  不是说那么多，说写就写！
+* 
+*/
+
+
+/* 先给自己模拟一个sublime Text吧 */
+
+.styleEditor {
+    font-size:1.2em;
+    width: 45vw;
+    height: 95vh;
+    border: 1px solid;
+    background: rgb(39, 40, 34);
+    color: rgb(248, 248, 242);
+    padding: 1em;
+    margin: .5em;
+    overflow: auto;
+    border-radius: .5em;
+}
+
+
+/* 作为程序员的编辑器，没有代码高亮怎么行？ */
+
+.token.comment {
+    color: rgb(104, 100, 83);
+}
+
+.token.selector {
+    color: rgb(249, 38, 95);
+}
+
+.token.property {
+    color: rgb(102, 217, 231);
+}
+
+.token.punctuation {
+    color: rgb(249, 151, 32);
+}
+
+.token.function {
+    color: rgb(166, 226, 44);
+}
+
+
+/* 还是不够炫酷，那就加点 3D 效果吧 */
+
+html {
+    -webkit-perspective: 1000px;
+    perspective: 1000px;
+}
+
+.styleEditor {
+    position: fixed;
+    left: 0;
+    top: 0;
+    transform: rotateY(10deg) translateZ(-100px);
+    -webkit-transform: rotateY(10deg) translateZ(-100px);
+}
+
+
+/* 接下来给自己准备一个MarkDown编辑器 */
+
+.resumeEditor {
+    position: fixed; right: 0; top: 0; 
+    width: 48vw; height: 95vh;
+    padding: 1em;
+    margin: .5em;
+    border: 1px solid;
+    background: white;
+    border-radius: .5em;
+}
+
+
+/* 还是一样来点 3D 效果 */
+
+.resumeEditor {
+    font-size:1.2em;
+    color: #222;
+    overflow: auto;
+    -webkit-transform: rotateY(-10deg) translateZ(-100px);
+    transform: rotateY(-10deg) translateZ(-100px);
+}
+
+
+/* 好了，我开始写简历了 */
+
+`,
+                `
+/* 这个简历好像差点什么
+ * 对了，这是 Markdown 格式的，我需要变成对 HR 更友好的格式
+ * 简单，用开源工具翻译成 HTML 就行了
+ */
+`,
+                `
+/* 再对 HTML 加点样式 */
+
+.resumeEditor h1 {
+    display: block;
+    text-align: center;
+    margin: 1em 0 .5em .25em;
+}
+
+.resumeEditor h2 {
+    display: inline-block;
+    border-bottom: 1px solid;
+    padding: .2em 0;
+    margin: 1em 0 .5em .25em;
+}
+
+.resumeEditor h3 {
+    display: block;
+    margin: 1em 0 .25em 1em;
+}
+
+.resumeEditor ul {
+    list-style: none;
+}
+
+.resumeEditor ul> li::before {
+    content: '•';
+    margin-right: .75em;
+}
+`
+            ],
+            myMarkdown: `# 罗德忠/lion1ou
+
+## 个人信息
+
+- 个人博客：[http://lion1ou.win](http://lion1ou.win)
+- Github：[https://github.com/lion1ou](https://github.com/lion1ou)
+- 详细简历：[http://lion1ou.win/resume/](http://lion1ou.win/resume/)
+- Email：lion1ou@163.com 
+
+## 项目经验
+
+### 上海建坤信息技术有限责任公司（2015.3 - 至今）
+
+* 建坤大型智慧园区管理平台 -- 工作流管理系统模块 
+* ble蓝牙环境监测APP、后端Node数据管理平台
+* 气象博物馆自动导览APP [下载链接](http://61.152.122.103/distribute/index.html)
+
+### 个人项目
+
+* 动画简历(本项目)[源码](https://github.com/lion1ou/resume)
+* 在线简历模板 [源码](https://github.com/lion1ou/resume)
+* TodoList [源码](https://github.com/lion1ou/TodoList)
+* 鸿福茶产业官网 [官网](http://www.fdshfccy.com/)
+
+## 技能栈
+
+- 前端技术：JavaScript / CSS3 / HTML5 / AJAX / ES6
+- Web开发：NodeJS / Koa / Express / JavaWeb
+- 前端框架：Bootstrap / Material / AngularJS / Ionic / VueJS
+- 前端工具：Gulp / webpack / grunt / SaSS / LeSS / PhoneGap
+- 数据库相关：MySQL / MongoDB
+- 版本管理、文档管理：Git / markdown
+- 工作平台：Mac / Windows
+
+`
+        }
+    },
+    created() {
+        this.codingResume();
+    },
+    methods: {
+
+        codingResume: async function() {
+
+            await this.codingShowStyle(0)
+            await this.codingShowResume();
+            await this.codingShowStyle(1);
+            await this.showHtml();
+            await this.codingShowStyle(2);
+        },
+        showHtml: function() {
+            return new Promise((resolve, reject) => {
+                this.enableHtml = true;
+                console.log(this.myStyle, this.myMarkdown);
+                resolve();
+            })
+        },
+        codingShowStyle: function(n) {
+            return new Promise((resolve, reject) => {
+                let intervalTime = this.intervalTime;
+                let showStyle = (async function() {
+                    let style = this.myStyle[n]
+                    if (!style) {
+                        return
+                    }
+                    let beforeNLength = 0;
+                    for (var i = 0; i < n + 1; i++) {
+
+                        beforeNLength += this.myStyle[i].length
+                        console.log(this.myStyle[i].length);
+                    }
+                    console.log('beforeNLength', beforeNLength);
+                    let fromLength = beforeNLength - style.length;
+                    console.log('fromLength', fromLength);
+                    if (this.currentStyle.length < beforeNLength) {
+                        let codingLength = this.currentStyle.length - fromLength;
+                        console.log('codingLength', codingLength);
+
+                        let char = style.substring(codingLength, codingLength + 1) || ' ';
+                        console.log(char);
+                        this.currentStyle += char;
+                        if (style.substring(codingLength - 1, codingLength) == '\n' && this.$refs.styleEditor) {
+                            this.$nextTick(() => {
+                                this.$refs.styleEditor.goBottom();
+                            })
+                        }
+                        setTimeout(showStyle, intervalTime)
+                    } else {
+                        resolve()
+                    }
+
+                }).bind(this)
+                showStyle()
+            })
+        },
+        codingShowResume: function() {
+            return new Promise((resolve, reject) => {
+                let intervalTime = this.intervalTime;
+                let length = this.myMarkdown.length;
+                let showResume = () => {
+                    if (this.currentMarkdown.length < length) {
+
+                        let char = this.myMarkdown.substring(this.currentMarkdown.length, this.currentMarkdown.length + 1) || ' ';
+                        console.log(char);
+
+                        this.currentMarkdown += char;
+                        if (this.myMarkdown.substring(length - 1, length) === '\n' && this.$refs.resumeEditor) {
+                            this.$nextTick(() => {
+                                this.$refs.resumeEditor.goBottom()
+                            })
+                        }
+                        setTimeout(showResume, intervalTime)
+                    } else {
+                        resolve()
+                    }
+                }
+                showResume()
+            })
+        }
+    }
+}
+</script>
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+html {
+    min-height: 100vh;
+}
+
+* {
+    transition: all .3s;
+    -webkit-transition: all .3s;
+}
+</style>
---
+++ src/components/styleEditor.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="styleEditor" ref="container">
+        <div class="styleCode" v-html="styleCode"></div>
+        <pre v-html="highlightedCode"></pre>
+    </div>
+</template>
+<script>
+import Prism from 'prismjs'
+export default {
+    name: 'styleEditor',
+    props: ['code'],
+    computed: {
+        styleCode: function() {
+            return `<style>${this.code}</style>`
+        },
+        highlightedCode: function() {
+            return Prism.highlight(this.code, Prism.languages.css)
+        }
+    },
+    methods: {
+        goBottom: function() {
+            this.$refs.container.scrollTop = 10000
+        }
+    }
+
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.styleCode {
+    display: none;
+}
+</style>
